4bd4ed30ea92314f8689833ad568bc3ab63d80a0
FlameWolf <agnijith@gmail.com>
2025-03-04 10:22:05 +0530

    Initial commit

---
+++ FileUploadHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Blobs.Specialized;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Client.Transport;
+
+namespace FileUploadSample
+{
+	public static class FileUploadHelper
+	{
+		public static async Task<FileUploadCompletionNotification> UploadFileAsync(string filePath, DeviceClient deviceClient)
+		{
+			using var fileStreamSource = new FileStream(filePath, FileMode.Open);
+			var fileUploadSasUriRequest = new FileUploadSasUriRequest
+			{
+				BlobName = Path.GetFileName(fileStreamSource.Name)
+			};
+			var sasUri = await deviceClient.GetFileUploadSasUriAsync(fileUploadSasUriRequest);
+			var fileUploadCompletionNotification = new FileUploadCompletionNotification
+			{
+				CorrelationId = sasUri.CorrelationId
+			};
+			try
+			{
+				var blockBlobClient = new BlockBlobClient(sasUri.GetBlobUri());
+				await blockBlobClient.UploadAsync(fileStreamSource, new BlobUploadOptions());
+				fileUploadCompletionNotification.IsSuccess = true;
+				fileUploadCompletionNotification.StatusCode = (int)HttpStatusCode.OK;
+				fileUploadCompletionNotification.StatusDescription = "Success";
+			}
+			catch (Exception ex)
+			{
+				fileUploadCompletionNotification.IsSuccess = false;
+				fileUploadCompletionNotification.StatusCode = (int)HttpStatusCode.InternalServerError;
+				fileUploadCompletionNotification.StatusDescription = ex.Message;
+			}
+			await deviceClient.CompleteFileUploadAsync(fileUploadCompletionNotification);
+			return fileUploadCompletionNotification;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
+
+namespace FileUploadSample
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var configuration = new ConfigurationBuilder()
+				.AddJsonFile("launchSettings.json", true, true)
+				.AddEnvironmentVariables()
+				.Build();
+			using var deviceClient = DeviceClient.CreateFromConnectionString(configuration["IOTHUB_DEVICE_CONN_STRING"]);
+			var result = await FileUploadHelper.UploadFileAsync("scenery.jpg", deviceClient);
+			Console.WriteLine($"File upload result: {JsonSerializer.Serialize(result)}");
+			await deviceClient.CloseAsync();
+		}
+	}
+}
